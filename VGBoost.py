2185a380b68e7dc58baca55e3382537b0b8d220d
Agnij Moitra <79319209+Agnij-Moitra@users.noreply.github.com>
2022-10-13 23:18:33 +0530

    initial classifier

--- VGBoost.py
+++ VGBoost.py
@@ -1,13 +1,14 @@
-from numpy import full
+import numpy as np
 from pandas import DataFrame, concat
 from numba import prange
 from concurrent.futures import ThreadPoolExecutor
 # Scikit-learn: Machine Learning in Python, Pedregosa et al., JMLR 12, pp. 2825-2830, 2011
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import RobustScaler, MinMaxScaler
-from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.metrics import mean_squared_error, r2_score, f1_score
 from sklearn.ensemble import GradientBoostingRegressor, HistGradientBoostingRegressor, BaggingRegressor, ExtraTreesRegressor
-from sklearn.svm import NuSVR
+from sklearn.svm import NuSVR, SVC
+from sklearn.base import BaseEstimator
 from lightgbm import LGBMRegressor
 from sklearn.neural_network import MLPRegressor
 from sklearn.linear_model import LinearRegression, BayesianRidge, ElasticNet, SGDRegressor, LassoLars, Lasso, Ridge, ARDRegression, RANSACRegressor, HuberRegressor, TheilSenRegressor, LassoLarsIC
@@ -17,15 +18,17 @@ from sklearn.neighbors import KNeighborsRegressor
 from xgboost import XGBRegressor
 from sklearn.impute import KNNImputer
 from time import perf_counter
+import collections.abc
 from copy import deepcopy
 
 
-class VGBRegressor(object):
+class VGBRegressor(BaseEstimator):
     """_summary_
 
     Args:
         object (_type_): _description_
     """
+
     def __init__(self):
         """ Initialize VGBRegressor Object
         """
@@ -153,12 +156,14 @@ class VGBRegressor(object):
                                 BaggingRegressor, NuSVR, XGBRegressor, SGDRegressor, KernelRidge, MLPRegressor, LGBMRegressor,
                                 Ridge, ARDRegression, RANSACRegressor, HuberRegressor, TheilSenRegressor, LassoLarsIC)
             elif light:
-                self._models = (LGBMRegressor, ExtraTreeRegressor, BaggingRegressor, RANSACRegressor, LassoLarsIC, BayesianRidge)
+                self._models = (LGBMRegressor, ExtraTreeRegressor,
+                                BaggingRegressor, RANSACRegressor, LassoLarsIC, BayesianRidge)
             else:
                 self._models = (DecisionTreeRegressor, LinearRegression, BayesianRidge, KNeighborsRegressor, LGBMRegressor,
                                 ElasticNet, LassoLars, Lasso, SGDRegressor, BaggingRegressor, ExtraTreesRegressor,
                                 Ridge, ARDRegression, RANSACRegressor, LassoLarsIC)
-        X_train = KNNImputer(weights='distance', n_neighbors=10).fit_transform(deepcopy(X_train))
+        X_train = KNNImputer(weights='distance',
+                             n_neighbors=10).fit_transform(deepcopy(X_train))
         self._y_mean = y_train.mean()
         # base model: mean
         # computer residuals: y - y hat
@@ -168,7 +173,7 @@ class VGBRegressor(object):
         # ada boost and adaptive scaling for learning rates
 
         preds = DataFrame(
-            data={'yt': y_train, 'p0': full((len(y_train)), y_train - self._y_mean)})
+            data={'yt': y_train, 'p0': np.full((len(y_train)), y_train - self._y_mean)})
         residuals = DataFrame(
             data={'r0': y_train - self._y_mean})
         errors = []
@@ -220,7 +225,7 @@ class VGBRegressor(object):
                                                 min_error_i], errors[:min_error_i]
         residuals = residuals[:len(errors)]
         return self._ensemble, (residuals, errors)
-    
+
     def predict(self, X_test):
         """_summary_
 
@@ -230,17 +235,263 @@ class VGBRegressor(object):
         Returns:
             _type_: _description_
         """
-        try: val = self._ensemble[0]
+        try:
+            val = self._ensemble[0]
         except Exception:
             return "Please train the model first"
         # X_test = self._robust.transform(self._minimax.transform(deepcopy(X_test)))
         preds = DataFrame(
-            data={'p0': full((len(X_test)), self._y_mean)})
+            data={'p0': np.full((len(X_test)), self._y_mean)})
         for i in prange(len(self._ensemble)):
             preds[f"p{i}"] = self._ensemble[i].predict(X_test)
         preds_ = preds.sum(axis=1)
         return preds_
-    
+
+    def score(self, X_test, y_true):
+        """
+        Args:
+            X_test (Iterable)
+            y_true (Iterable)
+
+        Returns:
+            float: R2 Score for y_true and y_predicted
+        """
+        return r2_score(y_true, self.predict(X_test))
+
+    def get_params(self):
+        return self.__dict__
+
+
+class VGBClassifier(BaseEstimator):
+    """_summary_
+
+    Args:
+        object (_type_): _description_
+    """
+
+    def __init__(self):
+        """ Initialize VGBRegressor Object
+        """
+        self._ensemble = []
+
+    def _metrics(self, vt, vp, model, time=None):
+        """_summary_
+
+        Args:
+            vt (_type_): _description_
+            vp (_type_): _description_
+            model (_type_): _description_
+            time (_type_, optional): _description_. Defaults to None.
+
+        Returns:
+            _type_: _description_
+        """
+        if self.custom_loss_metrics:
+            return {'model': model, 'time': time, 'loss': self.custom_loss_metrics(vt, vp)}
+        return {"model": model, "time": time, "loss": mean_squared_error(vt, vp)}
+
+    def _create_model(self, X, y, model_name, time_it: bool = False):
+        """_summary_
+
+        Args:
+            X (_type_): _description_
+            y (_type_): _description_
+            model_name (_type_): _description_
+            time_it (bool, optional): _description_. Defaults to False.
+
+        Returns:
+            _type_: _description_
+        """
+        model = model_name()
+        if time_it:
+            begin = perf_counter()
+            model.fit(X, y)
+            end = perf_counter()
+            return (model, end - begin)
+        return (model.fit(X, y), None)
+
+    def _get_metrics(self, model_name):
+        """_summary_
+
+        Args:
+            model_name (_type_): _description_
+
+        Returns:
+            _type_: _description_
+        """
+        try:
+            Xt, Xv, yt, yv = train_test_split(self._X, self._y)
+            results = self._create_model(Xt, yt, model_name, time_it=False)
+            model, time = results[0], results[1]
+            return self._metrics(yv,
+                                 model.predict(Xv), model, time)
+        except Exception:
+            return None
+
+    def _get_results(self, X, y) -> list:
+        """_summary_
+
+        Args:
+            X (_type_): _description_
+            y (_type_): _description_
+
+        Returns:
+            list: _description_
+        """
+        results = []
+        # self._X = self._minimax.fit_transform(self._robust.fit_transform(
+        #         KNNImputer(weights='distance').fit_transform(X)))
+        self._X = X
+        self._y = y
+        with ThreadPoolExecutor(max_workers=len(self._models)) as executor:
+            res = executor.map(self._get_metrics, self._models)
+            results = [i for i in res if i]
+        return results
+
+    def fit(
+        self, X_train, y_train,
+        early_stopping: bool = False,
+        early_stopping_min_delta: float = 0.001,
+        early_stopping_patience: int = 10,
+        custom_models: list = None,
+        learning_rate: float = 0.05,
+        n_estimators: int = 100,
+        warm_start: bool = False,
+        complexity: bool = False,
+        light: bool = True,
+        custom_loss_metrics: object = False,
+    ):
+        """_summary_
+
+        Args:
+            X_train (_type_): _description_
+            y_train (_type_): _description_
+            early_stopping (bool, optional): _description_. Defaults to False.
+            early_stopping_min_delta (float, optional): _description_. Defaults to 0.001.
+            early_stopping_patience (int, optional): _description_. Defaults to 10.
+            custom_models (list, optional): _description_. Defaults to None.
+            learning_rate (float, optional): _description_. Defaults to 0.05.
+            n_estimators (int, optional): _description_. Defaults to 100.
+            warm_start (bool, optional): _description_. Defaults to False.
+            complexity (bool, optional): _description_. Defaults to False.
+            custom_loss_metrics (object, optional): _description_. Defaults to False.
+
+        Returns:
+            _type_: _description_
+        """
+        if custom_models:
+            self._models = custom_models
+        self.custom_loss_metrics = custom_loss_metrics
+        self.learning_rate = learning_rate
+        self.n_estimators = n_estimators
+        self.early_stopping = early_stopping
+        self.early_stopping_min_delta = early_stopping_min_delta
+        self.early_stopping_patience = early_stopping_patience
+        if custom_models:
+            self._models = custom_models
+        else:
+            if complexity:
+                self._models = (DecisionTreeRegressor, LinearRegression, BayesianRidge, KNeighborsRegressor, HistGradientBoostingRegressor,
+                                ElasticNet, LassoLars, Lasso, GradientBoostingRegressor, ExtraTreesRegressor,
+                                BaggingRegressor, NuSVR, XGBRegressor, SGDRegressor, KernelRidge, MLPRegressor, LGBMRegressor,
+                                Ridge, ARDRegression, RANSACRegressor, HuberRegressor, TheilSenRegressor, LassoLarsIC)
+            elif light:
+                self._models = (LGBMRegressor, ExtraTreeRegressor,
+                                BaggingRegressor, RANSACRegressor, LassoLarsIC, BayesianRidge)
+            else:
+                self._models = (DecisionTreeRegressor, LinearRegression, BayesianRidge, KNeighborsRegressor, LGBMRegressor,
+                                ElasticNet, LassoLars, Lasso, SGDRegressor, BaggingRegressor, ExtraTreesRegressor,
+                                Ridge, ARDRegression, RANSACRegressor, LassoLarsIC)
+        X_train = KNNImputer(weights='distance',
+                             n_neighbors=10).fit_transform(deepcopy(X_train))
+        self._y_mean = y_train.mean()
+        # base model: mean
+        # computer residuals: y - y hat
+        # for n_estimators: a) y = prev residuals && residuals * learning rate
+        # add early stopping
+        # restore best weights
+        # ada boost and adaptive scaling for learning rates
+
+        preds = DataFrame(
+            data={'yt': y_train, 'p0': np.full((len(y_train)), y_train - self._y_mean)})
+        residuals = DataFrame(
+            data={'r0': y_train - self._y_mean})
+        errors = []
+        if not early_stopping:
+            if warm_start:
+                for i in prange(1, self.n_estimators + 1):
+                    y = residuals[f'r{i - 1}']
+                    results = self._get_results(X_train, y)
+                    min_loss = min(results, key=lambda x: x.get(
+                        "loss", float('inf')))["loss"]  # https://stackoverflow.com/a/19619294
+                    min_model = [i['model']
+                                 for i in results if min_loss >= i['loss']][0]
+                    preds[f'p{i}'] = residuals.sum(axis=1) + min_model.predict(
+                        X_train) * self.learning_rate
+                    residuals[f'r{i}'] = preds['yt'] - preds[f'p{i}']
+                    if i % 3 == 0:
+                        X_train[f"r{i}"] = residuals[f'r{i}'].copy()
+                    try:
+                        errors.append(mean_squared_error(
+                            preds['yt'], preds[f'p{i}']))
+                    except Exception:
+                        df = concat(
+                            [preds['yt'], preds[f'p{i - 1}']], axis=1).dropna()
+                        errors.append(mean_squared_error(
+                            df['yt'], df[f"p{i - 1}"]))
+                    self._ensemble.append(min_model)
+            else:
+                for i in prange(1, self.n_estimators + 1):
+                    y = residuals[f'r{i - 1}']
+                    results = self._get_results(X_train, y)
+                    min_loss = min(results, key=lambda x: x.get(
+                        "loss", float('inf')))["loss"]  # https://stackoverflow.com/a/19619294
+                    min_model = [i['model']
+                                 for i in results if min_loss >= i['loss']][0]
+                    preds[f'p{i}'] = residuals.sum(axis=1) + min_model.predict(
+                        X_train) * self.learning_rate
+                    residuals[f'r{i}'] = preds['yt'] - preds[f'p{i}']
+                    errors.append(mean_squared_error(
+                        preds['yt'], preds[f'p{i}']))
+                    self._ensemble.append(min_model)
+                    if errors[i - 1] == 0:
+                        break
+        else:
+            return "TODO"
+        min_error = min(errors)
+        min_error_i = [i for i in prange(
+            len(errors)) if errors[i] == min_error][0]
+        self._ensemble, errors = self._ensemble[:
+                                                min_error_i], errors[:min_error_i]
+        residuals = residuals[:len(errors)]
+        return self._ensemble, (residuals, errors)
+
+    def predict(self, X_test):
+        """_summary_
+
+        Args:
+            X_test (_type_): _description_
+
+        Returns:
+            _type_: _description_
+        """
+        try:
+            val = self._ensemble[0]
+        except Exception:
+            return "Please train the model first"
+        # X_test = self._robust.transform(self._minimax.transform(deepcopy(X_test)))
+        preds = DataFrame(
+            data={'p0': np.full((len(X_test)), self._y_mean)})
+        for i in prange(len(self._ensemble)):
+            preds[f"p{i}"] = self._ensemble[i].predict(X_test)
+        preds_ = preds.sum(axis=1)
+        def quantize(x):
+            if x > 0.5:
+                return 1
+            else:
+                return 0
+        return preds_.apply(quantize)
+
     def score(self, X_test, y_true):
         """
         Args:
